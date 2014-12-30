1a47459ed6b89179fbe06fd12a9a8bf2732f25db
Shun Takebayashi <shun@takebayashi.asia>
2014-12-30 11:28:07 +0900

    Add account validation

--- CocoaPinboard/PinboardClient.swift
+++ CocoaPinboard/PinboardClient.swift
@@ -24,7 +24,7 @@ SOFTWARE.
 
 import Foundation
 
-class PinboardClient {
+public class PinboardClient {
     
     let endpoint = "https://api.pinboard.in/v1"
     let queue = NSOperationQueue.mainQueue()
@@ -32,7 +32,7 @@ class PinboardClient {
     let username: String
     let token: String
     
-    init(username: String, token: String) {
+    public init(username: String, token: String) {
         self.username = username
         self.token = token
     }
@@ -43,25 +43,46 @@ class PinboardClient {
         return key + "=" + value.stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet())!
     }
     
-    func sendRequest(path: String, parameters: [String: String], callback: Callback) {
-        var params = parameters
+    public func validateToken(callback: (Bool, NSError?) -> Void) {
+        sendRequest("/user/api_token", parameters: nil) { json, error in
+            if let _ = error {
+                callback(false, error)
+            }
+            else if let result = json as? [String: String] {
+                if result["result"] == self.token {
+                    callback(true, nil);
+                }
+                else {
+                    callback(false, nil)
+                }
+            }
+            else {
+                callback(false, nil)
+            }
+        }
+    }
+    
+    func sendRequest(path: String, parameters: [String: String]?, callback: Callback) {
+        var params = parameters ?? [:]
         params["auth_token"] = username + ":" + token
+        params["format"] = "json"
         let qline = join("&", map(params, concatenateKeyAndValue))
         let url = NSURL(string: endpoint + path + "?" + qline)!
         let request = NSURLRequest(URL: url)
         NSURLConnection.sendAsynchronousRequest(request, queue: queue) { response, data, error in
-            if error != nil {
+            if let _ = error {
                 callback(nil, error)
-                return
             }
-            if let http = response as? NSHTTPURLResponse {
+            else if let http = response as? NSHTTPURLResponse {
                 if http.statusCode != 200 {
                     callback(nil, NSError(domain: "CocoaPinboardHTTPError", code: http.statusCode, userInfo:nil))
                 }
+                else {
+                    var jsonError: NSError?
+                    let json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError)
+                    callback(json, jsonError)
+                }
             }
-            var jsonError: NSError?
-            let json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError)
-            callback(json, jsonError)
         }
     }
     
