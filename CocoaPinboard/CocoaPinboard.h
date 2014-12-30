411804c9f42c0bbfbdf8c654e7e84ff49e47b8d9
Shun Takebayashi <shun@takebayashi.asia>
2014-12-30 11:45:10 +0900

    Add iOS framework

--- CocoaPinboard/CocoaPinboard.h
+++ CocoaPinboard/CocoaPinboard.h
@@ -22,7 +22,7 @@
  SOFTWARE.
  */
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 FOUNDATION_EXPORT double CocoaPinboardVersionNumber;
 FOUNDATION_EXPORT const unsigned char CocoaPinboardVersionString[];
